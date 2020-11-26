d5ac9b5273fe1025f636ef30bd253d1fdb771ea1
qnkhuat <qn.khuat@gmail.com>
2020-11-26 09:22:37 +0700

    It's a shame I'm solving this problem

--- classes-objects-testcases/solution.cpp
+++ classes-objects-testcases/solution.cpp
@@ -7,31 +7,51 @@
 using namespace std;
 
 // Write your Student class here
+class Student {
+	int scores[5];
+
+	public:
+	void input(){
+		for(int i = 0; i<5; i++){
+			cin>>scores[i];
+		}
+	}
+
+
+	int calculateTotalScore(){
+		int sum = 0;
+		for(int i=0; i<5; i++){
+			sum+=scores[i];
+		}
+		return sum;
+	}
+};
 
 int main() {
-    int n; // number of students
-    cin >> n;
-    Student *s = new Student[n]; // an array of n students
-    
-    for(int i = 0; i < n; i++){
-        s[i].input();
-    }
+	int n; // number of students
+	cin >> n;
+	Student *s = new Student[n]; // an array of n students
 
-    // calculate kristen's score
-    int kristen_score = s[0].calculateTotalScore();
+	for(int i = 0; i < n; i++){
+		s[i].input();
+	}
 
-    // determine how many students scored higher than kristen
-    int count = 0; 
-    for(int i = 1; i < n; i++){
-        int total = s[i].calculateTotalScore();
-        if(total > kristen_score){
-            count++;
-        }
-    }
+	// calculate kristen's score
+	int kristen_score = s[0].calculateTotalScore();
 
-    // print result
-    cout << count;
-    
-    return 0;
+	// determine how many students scored higher than kristen
+	int count = 0; 
+	for(int i = 1; i < n; i++){
+		int total = s[i].calculateTotalScore();
+		if(total > kristen_score){
+			count++;
+		}
+	}
+
+	// print result
+	cout << count;
+
+	return 0;
 }
+
 
